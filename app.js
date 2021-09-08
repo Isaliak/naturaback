8eb8e2aba9111e2482fbc344925f088ecca9d3be
Ismael Aliaga <ialiagakulat@gmail.com>
2021-09-08 16:39:50 -0400

    inicio del proyecto

---
+++ app.js
@@ -0,0 +1,8 @@
+const Server = require('./src/models/server')
+
+const server = new Server()
+
+// server.listen()
+
+
+
---
+++ src/models/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require("cors");
+
+
+
+class Server {
+    constructor() {
+        this.app = express()
+        this.port = process.env.PORT || 8000
+        this.routes()
+        this.middlewares()
+        this.listen()
+    }
+
+    routes() {
+        this.app.use('/api', require('../routes/users.routes'))
+    }
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(express.json())
+
+    }
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server runing in http://localhost:` + this.port);
+        });
+    }
+}
+
+
+module.exports = Server
