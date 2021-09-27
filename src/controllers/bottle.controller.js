a8fe3c6d1ab3de1f7202d8c7109ee10550a4a269
Ismael Aliaga <ialiagakulat@gmail.com>
2021-09-27 17:31:13 -0400

    creacio de los nuevos modelos y el log

--- src/controllers/bottle.controller.js
+++ src/controllers/bottle.controller.js
@@ -9,17 +9,17 @@ const bottleGet = async (req, res) => {
         const resp = await bottle_type.findAll({ where: { deleted: false } })
         return (resp != null && resp.length != 0)
             ? (
-                logCreate('successful', 'get', 'bottleGet', 'successful', 200, `${resp}`, req),
+                logCreate('successful', 'get', 'bottleGet', 'successful', 200, JSON.stringify(resp), req),
                 res.status(200).json(resp)
             )
             : (
-                logCreate('successful', 'get', 'bottleGet', 'failed', 400, `${resp}`, req),
+                logCreate('successful', 'get', 'bottleGet', 'failed', 400, JSON.stringify(resp), req),
                 res.status(400).json({ error: 'fallo al recuperar el registro revise los datos' })
             )
     } catch (error) {
         msg = { 'error': error, 'msg': error.message }
         console.log(msg);
-        logCreate('successful', 'get', 'bottleGet', `${msg}`, 500, `${error}`, req)
+        logCreate('successful', 'get', 'bottleGet', JSON.stringify(msg), 500, JSON.stringify(error), req)
         return res.status(500).json(msg)
     }
 }
@@ -30,16 +30,16 @@ const bottleGetById = async (req, res) => {
         return (resp != null && resp.length != 0)
             ? (
                 res.status(200).json(resp),
-                logCreate('successful', 'get', 'bottleGet', 'successful', 200, `${resp}`, req)
+                logCreate('successful', 'get', 'bottleGetById', 'successful', 200, JSON.stringify(resp), req)
             )
             : (
                 res.status(201).json({ error: 'fallo al recuperar el registro revise los datos' }),
-                logCreate('successful', 'get', 'bottleGet', 'failed', 400, `${resp}`, req)
+                logCreate('error', 'get', 'bottleGetById', 'failed', 400, JSON.stringify(resp), req)
             )
     } catch (error) {
         msg = { 'error': error, 'msg': error.message }
         console.log(msg);
-        logCreate('successful', 'get', 'bottleGet', `${msg}`, 400, `${error}`, req)
+        logCreate('error', 'get', 'bottleGetById', JSON.stringify(msg), 400, JSON.stringify(error), req)
         return res.status(500).json({ msg })
     }
 }
@@ -49,10 +49,19 @@ const bottleCreate = async (req = request, res = response) => {
         const resp = await bottle_type.create(
             { name, value, createdAt: new Date(), updatedAt: new Date() }
         )
-        return (resp != null && resp.length != 0) ? res.status(201).json(resp) : res.status(201).json({ error: 'fallo al realizar el registro revise los datos' })
+        return (resp != null && resp.length != 0)
+            ? (
+                res.status(201).json(resp),
+                logCreate('successful', 'post', 'bottleCreate', 'successful', 201, JSON.stringify(resp), req)
+            )
+            : (
+                res.status(201).json({ error: 'fallo al realizar el registro revise los datos' }),
+                logCreate('error', 'post', 'bottleCreate', 'failed', 400, JSON.stringify(resp), req)
+            )
     } catch (error) {
         msg = { 'error': error, 'msg': error.message }
         console.log(msg);
+        logCreate('error', 'post', 'bottleCreate', JSON.stringify(msg), 500, JSON.stringify(error), req)
         return res.status(500).json({ msg })
     }
 }
@@ -63,10 +72,19 @@ const bottleUpdate = async (req, res) => {
         const resp = await bottle_type.update(
             { name, value, state, updatedAt: new Date() },
             { where: { id: id } })
-        return (resp != null && resp != 0) ? res.status(201).json(resp) : res.status(400).json({ error: 'fallo al actualizar el registro revise los datos' })
+        return (resp != null && resp != 0)
+            ? (
+                res.status(201).json(resp),
+                logCreate('successful', 'put', 'bottleUpdate', 'successful', 201, JSON.stringify(resp), req)
+            )
+            : (
+                res.status(201).json({ error: 'fallo al actualizar el registro revise los datos' }),
+                logCreate('error', 'put', 'bottleUpdate', 'failed', 400, JSON.stringify(resp), req)
+            )
     } catch (error) {
         msg = { 'error': error, 'msg': error.message }
         console.log(msg);
+        logCreate('error', 'put', 'bottleUpdate', JSON.stringify(msg), 500, JSON.stringify(error), req)
         return res.status(500).json({ msg })
     }
 }
@@ -76,10 +94,19 @@ const bottleDelete = async (req, res) => {
         const resp = await bottle_type.update(
             { deleted: true, updatedAt: new Date() },
             { where: { id: id } })
-        return (resp != null && resp != 0) ? res.status(201).json(resp) : res.status(400).json({ error: 'fallo al eliminar el registro revise los datos' })
+        return (resp != null && resp != 0)
+            ? (
+                res.status(201).json(resp),
+                logCreate('successful', 'put', 'bottleDelete', 'successful', 201, JSON.stringify(resp), req)
+            )
+            : (
+                res.status(201).json({ error: 'fallo al eliminar el registro revise los datos' }),
+                logCreate('error', 'put', 'bottleDelete', 'failed', 400, JSON.stringify(resp), req)
+            )
     } catch (error) {
         msg = { 'error': error, 'msg': error.message }
         console.log(msg);
+        logCreate('error', 'put', 'bottleDelete', JSON.stringify(msg), 500, JSON.stringify(error), req)
         return res.status(500).json({ msg })
     }
 }
